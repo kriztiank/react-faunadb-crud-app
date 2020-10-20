9b6e88d6fab7aefff34b3eb9d3ce904d5c0bf62a
kriztiank <kriztiank@gmail.com>
2020-10-20 12:58:22 +0200

    changed /api to /.netlify/functions

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import LinkList from './components/LinkList';
 import LinkForm from './components/LinkForm';
+import LinkList from './components/LinkList';
 
 //Grab all of the links
 //display all of the links
 //add delete and archive functionality
 // App > LinkList > LinkCard
 function App() {
-    const [links, setLinks] = useState([]);
-    const loadLinks = async () => {
-        try {
-            const res = await fetch('/api/getLinks');
-            const links = await res.json();
-            setLinks(links);
-        } catch (err) {
-            console.error(err);
-        }
-    };
+  const [links, setLinks] = useState([]);
+  const loadLinks = async () => {
+    try {
+      const res = await fetch('/.netlify/functions/getLinks');
+      const links = await res.json();
+      setLinks(links);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-    useEffect(() => {
-        loadLinks();
-    }, []);
+  useEffect(() => {
+    loadLinks();
+  }, []);
 
-    return (
-        <div className="container py-5">
-            <h1 className="text-center mb-5">List O' Link</h1>
-            <LinkForm refreshLinks={loadLinks} />
-            {/* pass links and refreshLinks */}
-            <LinkList links={links} refreshLinks={loadLinks} />
-        </div>
-    );
+  return (
+    <div className='container py-5'>
+      <h1 className='text-center mb-5'>List O' Link</h1>
+      <LinkForm refreshLinks={loadLinks} />
+      {/* pass links and refreshLinks */}
+      <LinkList links={links} refreshLinks={loadLinks} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 export default function LinkForm({ refreshLinks }) {
-  const [name, setName] = useState("");
-  const [url, setUrl] = useState("");
-  const [description, setDescription] = useState("");
+  const [name, setName] = useState('');
+  const [url, setUrl] = useState('');
+  const [description, setDescription] = useState('');
 
   const resetForm = () => {
-    setName("");
-    setDescription("");
-    setUrl("");
+    setName('');
+    setDescription('');
+    setUrl('');
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const body = { name, url, description };
     try {
-      await fetch("/api/createLink", {
-        method: "POST",
+      await fetch('/.netlify/functions/createLink', {
+        method: 'POST',
         body: JSON.stringify(body),
       });
       resetForm();
@@ -27,40 +27,40 @@ export default function LinkForm({ refreshLinks }) {
   };
 
   return (
-    <div className="card">
-      <div className="card-header">Add link</div>
-      <div className="card-body">
+    <div className='card'>
+      <div className='card-header'>Add link</div>
+      <div className='card-body'>
         <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
+          <div className='form-group'>
+            <label htmlFor='name'>Name</label>
             <input
-              type="text"
-              name="name"
-              className="form-control"
+              type='text'
+              name='name'
+              className='form-control'
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="url">URL</label>
+          <div className='form-group'>
+            <label htmlFor='url'>URL</label>
             <input
-              type="text"
-              name="url"
-              className="form-control"
+              type='text'
+              name='url'
+              className='form-control'
               value={url}
               onChange={(e) => setUrl(e.target.value)}
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="description">Description</label>
+          <div className='form-group'>
+            <label htmlFor='description'>Description</label>
             <textarea
-              name="description"
-              className="form-control"
+              name='description'
+              className='form-control'
               value={description}
               onChange={(e) => setDescription(e.target.value)}
             />
           </div>
-          <button type="submit" className="btn btn-primary">
+          <button type='submit' className='btn btn-primary'>
             Submit
           </button>
         </form>
